84e1b868d3b72173385a7fbdb69a9c52406d40cf
Lucas Sanches <sanchesluka31@gmail.com>
2025-03-03 19:00:34 -0300

    Logic for mobile and desktop

--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
-import {ProjectCardComponent} from "../../components/project-card/project-card.component";
+import {Component, HostListener, Inject, PLATFORM_ID} from '@angular/core';
+import {
+  ProjectCardSlidesDesktopComponent
+} from "../../components/project-card/project-card-slides-desktop/project-card-slides-desktop.component";
+import {
+  ProjectCardSlidesMobileComponent
+} from "../../components/project-card/project-card-slides-mobile/project-card-slides-mobile.component";
+import {isPlatformBrowser, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-projects-page',
   standalone: true,
   imports: [
-    ProjectCardComponent
+    ProjectCardSlidesDesktopComponent,
+    ProjectCardSlidesMobileComponent,
+    NgIf
   ],
   templateUrl: './projects-page.component.html',
-  styleUrl: './projects-page.component.scss'
+  styleUrls: ['./projects-page.component.scss', '../base.scss']
 })
 export class ProjectsPageComponent {
+  public isDesktop: boolean;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isDesktop = this.isPlatformBrowser() && window.innerWidth > 768;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: any): void {
+    if (this.isPlatformBrowser()) {
+      this.isDesktop = event.target.innerWidth > 768;
+    }
+  }
+
+  private isPlatformBrowser(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
 }
